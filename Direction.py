7c7640b54aed8d615e9e26fcd7669b0dc74f5e9c
alexandre <alexandre.debeaumont@epitech.eu>
2019-11-21 12:57:53 -0500

    Player can no longer walk back to its previous direction

--- Direction.py
+++ Direction.py
@@ -6,3 +6,17 @@ class Direction(enum.Enum):
     RIGHT = 1
     DOWN = 2
     LEFT = 3
+    NULL = 4
+
+    def is_movement_valid(prev, new):
+        if new == Direction.NULL:
+            return False
+        if prev == Direction.UP and new == Direction.DOWN:
+            return False
+        if prev == Direction.DOWN and new == Direction.UP:
+            return False
+        if prev == Direction.LEFT and new == Direction.RIGHT:
+            return False
+        if prev == Direction.RIGHT and new == Direction.LEFT:
+            return False
+        return True
